d002670e2decb2b7e23cc01947a716b9554d495e
Veli Arda Balcı <veliarda.balci@gmail.com>
2024-12-28 23:17:33 +0300

    chrome path added

--- internal/services/screenshot_service.go
+++ internal/services/screenshot_service.go
@@ -10,52 +10,46 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-// CaptureScreenshot belirli bir URL'nin ekran görüntüsünü alır
 func CaptureScreenshot(url string) (string, error) {
 	// Ekran görüntüsü dosya yolunu belirle
+	fmt.Println("geldi2")
+
 	fileName := generateFileName(url) + ".png"
 	outputDir := "screenshots"
-
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
 		return "", fmt.Errorf("failed to create screenshots directory: %w", err)
 	}
 	filePath := filepath.Join(outputDir, fileName)
+	fmt.Println("geldi3")
 
-	// Tarayıcı binary'sini belirtin (gerektiğinde özelleştirin)
-	chromePath := "/usr/bin/google-chrome"
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.ExecPath(chromePath), // Tarayıcı binary yolu
-		chromedp.Headless,             // Headless mod
-		chromedp.DisableGPU,           // GPU'yu devre dışı bırak
-		chromedp.NoSandbox,            // Sandbox devre dışı (sunucularda gerekebilir)
-	)
-
-	// Context oluştur ve timeout ekle
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	// Chrome context oluştur
+	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	ctx, cancel = chromedp.NewContext(ctx)
-	defer cancel()
-
+	// Timeout ekle
 	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
+	fmt.Println("geldi4")
 
 	// Ekran görüntüsü al
 	var buf []byte
-	if err := chromedp.Run(ctx, chromedp.Tasks{
+	err := chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.Navigate(url),
 		chromedp.FullScreenshot(&buf, 90),
-	}); err != nil {
+	})
+	if err != nil {
 		return "", fmt.Errorf("failed to capture screenshot: %w", err)
 	}
+	fmt.Println("geldi5")
 
 	// Görüntüyü dosyaya kaydet
 	if err := os.WriteFile(filePath, buf, 0644); err != nil {
 		return "", fmt.Errorf("failed to save screenshot: %w", err)
 	}
+	fmt.Println("çıkış")
 
-	fmt.Printf("Screenshot captured successfully: %s\n", filePath)
-	return fileName, nil
+	filePath = fileName
+	return filePath, nil
 }
 
 // generateFileName: URL'den hashlenmiş bir dosya adı oluşturur
