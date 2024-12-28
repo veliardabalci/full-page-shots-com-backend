f933adb74794a239a48557bae60fa7e382bfddf3
Veli Arda Balcı <veliarda.balci@gmail.com>
2024-12-28 23:46:33 +0300

    chrome path change

--- internal/services/screenshot_service.go
+++ internal/services/screenshot_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -11,45 +12,52 @@ import (
 )
 
 func CaptureScreenshot(url string) (string, error) {
-	// Ekran görüntüsü dosya yolunu belirle
-	fmt.Println("geldi2")
+	fmt.Println("Ekran görüntüsü alımı başlatıldı: ", url)
 
+	// Ekran görüntüsü dosya yolunu belirle
 	fileName := generateFileName(url) + ".png"
 	outputDir := "screenshots"
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-		return "", fmt.Errorf("failed to create screenshots directory: %w", err)
+		return "", fmt.Errorf("Klasör oluşturulamadı: %w", err)
 	}
 	filePath := filepath.Join(outputDir, fileName)
-	fmt.Println("geldi3")
 
 	// Chrome context oluştur
-	ctx, cancel := chromedp.NewContext(context.Background())
+	opts := chromedp.DefaultExecAllocatorOptions[:]
+	opts = append(opts,
+		chromedp.Headless,   // Başsız mod
+		chromedp.DisableGPU, // GPU'yu devre dışı bırak
+		chromedp.NoSandbox,  // Sanal alanı devre dışı bırak
+		chromedp.Flag("disable-software-rasterizer", true),
+	)
+	// Chrome context oluştur
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
 	defer cancel()
 
 	// Timeout ekle
-	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	timeoutCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
-	fmt.Println("geldi4")
 
 	// Ekran görüntüsü al
 	var buf []byte
-	err := chromedp.Run(ctx, chromedp.Tasks{
+	err := chromedp.Run(timeoutCtx, chromedp.Tasks{
 		chromedp.Navigate(url),
 		chromedp.FullScreenshot(&buf, 90),
 	})
 	if err != nil {
-		return "", fmt.Errorf("failed to capture screenshot: %w", err)
+		return "", fmt.Errorf("Ekran görüntüsü alınamadı: %w", err)
 	}
-	fmt.Println("geldi5")
 
 	// Görüntüyü dosyaya kaydet
 	if err := os.WriteFile(filePath, buf, 0644); err != nil {
-		return "", fmt.Errorf("failed to save screenshot: %w", err)
+		return "", fmt.Errorf("Ekran görüntüsü kaydedilemedi: %w", err)
 	}
-	fmt.Println("çıkış")
 
-	filePath = fileName
-	return filePath, nil
+	fmt.Println("Ekran görüntüsü başarıyla alındı: ", filePath)
+	return fileName, nil
 }
 
 // generateFileName: URL'den hashlenmiş bir dosya adı oluşturur
